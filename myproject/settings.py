a603676045206e620e06ef0d401267f69a3aca1c
Park-Seonhyeong <Park-Seonhyeong>
2022-02-18 17:24:25 +0900

    setting change2

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-**-qi49kh%!xm)&h57%jj%p01m(4fjsdk%c90-b45t(gwe_uoj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hongiklikelion.pythonanywhere.com']
+ALLOWED_HOSTS = ['honiklikelion10.pythonanywhere.com']
 
 AUTH_USER_MODEL='accounts.CustomUser'
 
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
